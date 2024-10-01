2d1fa308e2f1be60ecc2ae8f78e89439fcdf37f4
jvtubergen <jvtubergen@pm.me>
2024-10-01 18:17:01 +0200

    Update pcm_bin and pcm_pyo3 for linear partial_curve_graph implementation.

--- pcm_pyo3/src/lib.rs
+++ pcm_pyo3/src/lib.rs
@@ -4,9 +4,8 @@ use pcm::prelude::*;
 #[pymodule]
 fn partial_curve_matching(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Vector>()?;
-    m.add_class::<Graph>()?;
-    m.add_function(wrap_pyfunction!(partial_curve, m)?)?;
+    m.add_class::<LinearGraph>()?;
+    m.add_function(wrap_pyfunction!(make_linear_graph, m)?)?;
     m.add_function(wrap_pyfunction!(partial_curve_graph_linear, m)?)?;
-    m.add_function(wrap_pyfunction!(make_graph, m)?)?;
     Ok(())
 }
